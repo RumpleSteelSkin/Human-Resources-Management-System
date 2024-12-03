8f198de9174c27ad67617f6f645596fe047870f5
ASLIHAN <69537329+aslihanavsar@users.noreply.github.com>
2024-12-03 12:27:17 +0300

    Position ve Department classları eklendi.

---
+++ HRMS.Entities/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using HRMS.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Entities.Models
+{
+    public class Department : BaseEntity
+    {
+        public int DepartmentID { get; set; }
+        public string? DepartmentName { get; set; }
+        public string? DepartmentLocation { get; set; }
+
+    }
+}
---
+++ HRMS.Entities/Models/Position.cs
@@ -0,0 +1,17 @@
+﻿using HRMS.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Entities.Models
+{
+    public class Position : BaseEntity
+    {
+        public int PositionID { get; set; }
+        public string? PositionName { get; set; }
+        public decimal PositionsBaseSalary { get; set; }
+
+    }
+}
