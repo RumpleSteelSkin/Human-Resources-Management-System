f0a795b43ba3f7223b5fd83562d79729ebcdfd2b
RumpleSteelSkin <osmnistbayrak@gmail.com>
2024-12-03 17:26:37 +0300

    Add ADBContext.cs and migrate it

---
+++ HRMS.DataAccess/Context/ADBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HRMS.Entities.Models;
+
+namespace HRMS.DataAccess.Context
+{
+    public class ADBContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<PerformanceReview> PerformanceReviews { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<TrainingProgram> TrainingPrograms { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=HRMS;Integrated Security=True;Trust Server Certificate=True;User Id=Admin;Password=n^9Fu:,ek?$Wg01sU$I}6N9%)#JIQX8g");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PerformanceReview>()
+                .HasOne(pr => pr.Employee)
+                .WithMany()
+                .HasForeignKey(pr => pr.EmployeeID)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            modelBuilder.Entity<PerformanceReview>()
+                .HasOne(pr => pr.Review)
+                .WithMany()
+                .HasForeignKey(pr => pr.ReviewID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ HRMS.DataAccess/Migrations/20241203141724_FirstInitialize.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstInitialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HireDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TerminationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DepartmentID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PositionID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Subordinate = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    TrainingProgramID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalTable: "Departments",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Employees_Positions_PositionID",
+                        column: x => x.PositionID,
+                        principalTable: "Positions",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveRequests",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LeaveType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LeaveStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveRequests", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequests_Employees_EmployeeID",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PerformanceReviews",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReviewDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReviewID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PerformanceReviews", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PerformanceReviews_Employees_EmployeeID",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PerformanceReviews_Employees_ReviewID",
+                        column: x => x.ReviewID,
+                        principalTable: "Employees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrainingPrograms",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TrainerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainingPrograms", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_TrainingPrograms_Employees_TrainerID",
+                        column: x => x.TrainerID,
+                        principalTable: "Employees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentID",
+                table: "Employees",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_PositionID",
+                table: "Employees",
+                column: "PositionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_TrainingProgramID",
+                table: "Employees",
+                column: "TrainingProgramID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequests_EmployeeID",
+                table: "LeaveRequests",
+                column: "EmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PerformanceReviews_EmployeeID",
+                table: "PerformanceReviews",
+                column: "EmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PerformanceReviews_ReviewID",
+                table: "PerformanceReviews",
+                column: "ReviewID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainingPrograms_TrainerID",
+                table: "TrainingPrograms",
+                column: "TrainerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_TrainingPrograms_TrainingProgramID",
+                table: "Employees",
+                column: "TrainingProgramID",
+                principalTable: "TrainingPrograms",
+                principalColumn: "ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentID",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Positions_PositionID",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_TrainingPrograms_TrainingProgramID",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "LeaveRequests");
+
+            migrationBuilder.DropTable(
+                name: "PerformanceReviews");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+
+            migrationBuilder.DropTable(
+                name: "TrainingPrograms");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
