30b2d43627d441c701b4838264d70cbc22a3f023
RumpleSteelSkin <osmnistbayrak@gmail.com>
2024-12-03 10:25:23 +0300

    Add Abstractions folder and BaseEntity

---
+++ HRMS.Entities/Abstractions/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace HRMS.Entities.Abstractions
+{
+    public abstract class BaseEntity
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
