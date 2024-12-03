74e82c1756f13c9a4a22b7ab8ab33e80b222f494
ASLIHAN <69537329+aslihanavsar@users.noreply.github.com>
2024-12-03 12:39:19 +0300

    Position ve Department classlarında ufak düzenlemeler yapıldı.

--- HRMS.Entities/Models/Department.cs
+++ HRMS.Entities/Models/Department.cs
@@ -1,17 +1,10 @@
 ﻿using HRMS.Entities.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace HRMS.Entities.Models
 {
     public class Department : BaseEntity
     {
-        public int DepartmentID { get; set; }
-        public string? DepartmentName { get; set; }
-        public string? DepartmentLocation { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
 
     }
 }
--- HRMS.Entities/Models/Position.cs
+++ HRMS.Entities/Models/Position.cs
@@ -1,17 +1,10 @@
 ﻿using HRMS.Entities.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace HRMS.Entities.Models
 {
     public class Position : BaseEntity
     {
-        public int PositionID { get; set; }
-        public string? PositionName { get; set; }
-        public decimal PositionsBaseSalary { get; set; }
+        public string? Name { get; set; }
+        public decimal Salary { get; set; }
 
     }
 }
