fa8b5f1065b2a550a4cf0d6fdeb8abc6a17276e0
metinhan <kmetn7@gmail.com>
2024-12-03 14:17:38 +0300

    Employee ve TraningProgram Entityleri girildi.

---
+++ HRMS.Entities/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using HRMS.Entities.Abstractions;
+namespace HRMS.Entities.Models
+{
+    public class Employee : BaseEntity
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime? TerminationDate { get; set; } = null;
+        public decimal Salary { get; set; }
+
+
+    }
+}
---
+++ HRMS.Entities/Models/TrainingProgram.cs
@@ -0,0 +1,13 @@
+﻿using HRMS.Entities.Abstractions;
+
+namespace HRMS.Entities.Models
+{
+    public class TrainingProgram : BaseEntity
+    {
+        public string? Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Description { get; set; }
+        
+    }
+}
