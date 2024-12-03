2f51278d0afad62f645e3ac618f1e8b1aea2317f
Eren GENÇ <erengenc.9414@gmail.com>
2024-12-03 11:51:29 +0300

    LeaveRequests ve PerformanceReviews classları temel olarak oluşturuldu.

---
+++ HRMS.Entities/Models/PerformanceReviews.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Entities.Models
+{
+    public class PerformanceReviews
+    {
+        public DateTime ReviewDate { get; set; }
+        public int Score { get; set; }
+        public string Comments { get; set; }
+    }
+}
