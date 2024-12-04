55c2b56c0cc194c17d9845578692f8542928dcc8
Eren GENÇ <erengenc.9414@gmail.com>
2024-12-04 16:16:16 +0300

    LeaveRequestRepository ve PerformanceReviewsRepositoryde ki ufak hatalar giderildi. GenericRepositoryden türerildi.

--- HRMS.DataAccess/Repositories/LeaveRequestRepository.cs
+++ HRMS.DataAccess/Repositories/LeaveRequestRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HRMS.DataAccess.Context;
+using HRMS.Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace HRMS.DataAccess.Repositories
 {
-    internal class LeaveRequestRepository
+    public class LeaveRequestRepository(ADBContext context) : GenericRepository<Employee>(context)
     {
     }
 }
