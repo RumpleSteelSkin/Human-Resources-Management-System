2a4a4f8f0bd0e001953b0fd9e4ccd7dfee389d17
ASLIHAN <69537329+aslihanavsar@users.noreply.github.com>
2024-12-04 17:30:07 +0300

    Position Repository oluşturuldu.

---
+++ HRMS.DataAccess/Repositories/PositionRepository.cs
@@ -0,0 +1,8 @@
+﻿using HRMS.DataAccess.Context;
+using HRMS.Entities.Models;
+namespace HRMS.DataAccess.Repositories
+{
+    public class PositionRepository(ADBContext context) : GenericRepository <Position>(context)
+    {
+    }
+}
