c4fed1ef73aa839c29a99d60de98a74762583ea1
metinhan <kmetn7@gmail.com>
2024-12-04 17:31:10 +0300

    employeeValidator oluşturulup dolduruldu.

--- HRMS.Business/Validators/EmployeeValidator.cs
+++ HRMS.Business/Validators/EmployeeValidator.cs
@@ -7,7 +7,27 @@ namespace HRMS.Business.Validators
     {
         public EmployeeValidator()
         {
-            
+            RuleFor(x => x.FullName)
+              .NotNull().NotEmpty().WithMessage("Çalışan isim soyisim boş geçilemez.")
+              .MinimumLength(5).WithMessage("Çalışanın isim soyisim minimum 3 karakter olmalıdır.");
+
+
+            RuleFor(x => x.DateOfBirth)
+                .NotNull().NotEmpty().WithMessage("Çalışanın doğum tarihi boş geçilemez.");
+
+            RuleFor(x => x.Gender)
+                .NotNull().NotEmpty().WithMessage("Çalışanın cinsiyeti boş geçilemez.");
+
+            RuleFor(x => x.HireDate)
+                .NotNull().NotEmpty().WithMessage("Çalışanın işe giriş tarihi girilmesi gerekmektedir.")
+                .GreaterThanOrEqualTo(DateTime.Now).WithMessage("çalışanın işe giriş tarihi gelecekte bir tarihte olamaz.");
+            RuleFor(x => x.TerminationDate)
+                .NotNull().NotEmpty().WithMessage("Çalışanın işten ayrılış tarihi girilmesi gerekmektedir.")
+                .GreaterThanOrEqualTo(DateTime.Now).WithMessage("çalışanın işten ayrılış tarihi gelecekte bir tarihte olamaz.");
+            RuleFor(x => x.Salary)
+                .NotNull().NotEmpty().WithMessage("Çalışanın maaşı belirlenmelidir boş geçilemez.");
+
+
         }
     }
 }
