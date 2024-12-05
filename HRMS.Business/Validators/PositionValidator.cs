02c7a3071301dccd36105e470a5202e822ba39bf
ASLIHAN <69537329+aslihanavsar@users.noreply.github.com>
2024-12-05 15:59:19 +0300

    Position Validator oluşturuldu.

---
+++ HRMS.Business/Validators/PositionValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using HRMS.Entities.Models;
+
+namespace HRMS.Business.Validators
+{
+    public class PositionValidator : AbstractValidator<Position>
+    {
+        public PositionValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull().NotEmpty().WithMessage("Pozisyon adı boş geçilemez.")
+                .MinimumLength(3).WithMessage("Pozisyon adı en az 3 karakter olmalıdır.");
+
+            RuleFor(x => x.Salary)
+                .NotNull().NotEmpty().WithMessage("Pozisyon başlangıç maaşı boş bırakılamaz.")
+                .GreaterThan(0).WithMessage("Pozisyon başlangıç maaşı 0'dan büyük olmalıdır.");
+        }
+    }
+}
