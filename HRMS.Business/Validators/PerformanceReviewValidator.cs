cf58653bf81ed2f4fdc21f1d47913f17c5e941c8
Eren GENÇ <erengenc.9414@gmail.com>
2024-12-05 13:40:18 +0300

    PerformanceReview Validator dosyası oluşturuldu.

---
+++ HRMS.Business/Validators/PerformanceReviewValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using HRMS.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Business.Validators
+{
+    public class PerformanceReviewValidator : AbstractValidator<PerformanceReview>
+    {
+        public PerformanceReviewValidator() 
+        { 
+            RuleFor(x => x.ReviewDate)
+                .NotEmpty().WithMessage("İnceleme tarihini lütfen boş geçmeyiniz.");
+
+            RuleFor(x => x.Score)
+                .NotEmpty().WithMessage("Performans puanını giriniz.");
+
+            RuleFor(x => x.Comments)
+                .Length(100).WithMessage("Yorum ekleyebilirsiniz.");
+        }
+    }
+}
