9a1fdfb1ff99b320ddc718c31d380df62370b7e5
ASLIHAN <69537329+aslihanavsar@users.noreply.github.com>
2024-12-05 15:57:17 +0300

    Department Validator oluşturuldu.

---
+++ HRMS.Business/Validators/DepartmentValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using HRMS.Entities.Models;
+
+namespace HRMS.Business.Validators
+{
+    public class DepartmentValidator : AbstractValidator<Department>
+    {
+        public DepartmentValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull().NotEmpty().WithMessage("Departman adı boş geçilemez.")
+                .MinimumLength(3).WithMessage("Departman adı en az 3 karakter olmalıdır.");
+
+            RuleFor(x => x.Location)
+                .NotNull().NotEmpty().WithMessage("Departman lokasyonu boş geçilemez.");
+        }
+    }
+}
