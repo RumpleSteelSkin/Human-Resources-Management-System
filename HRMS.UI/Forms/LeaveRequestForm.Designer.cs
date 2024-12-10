ab41396f64e628daf4c8c71a111b8e901148e80e
Eren GENÇ <erengenc.9414@gmail.com>
2024-12-10 14:35:24 +0300

    LeaveRequest formu oluşturuldu.

--- HRMS.UI/Forms/LeaveRequestForm.Designer.cs
+++ HRMS.UI/Forms/LeaveRequestForm.Designer.cs
@@ -28,12 +28,348 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LeaveRequestForm";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            izinCombo = new ComboBox();
+            label9 = new Label();
+            label10 = new Label();
+            textBox6 = new TextBox();
+            yorumtxt = new TextBox();
+            yorumlbl = new Label();
+            label13 = new Label();
+            label12 = new Label();
+            endDatePicker = new DateTimePicker();
+            startDatePicker = new DateTimePicker();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(244, 41);
+            label1.TabIndex = 0;
+            label1.Text = "İzin talep formu";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.Location = new Point(5, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(774, 41);
+            label2.TabIndex = 1;
+            label2.Text = "---------------------------------------------------------------\r\n";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label3.Location = new Point(12, 108);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Çalışan ismi";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 18F);
+            textBox1.Location = new Point(12, 148);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(338, 49);
+            textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F);
+            label4.Location = new Point(12, 200);
+            label4.Name = "label4";
+            label4.Size = new Size(36, 20);
+            label4.TabIndex = 4;
+            label4.Text = "İsim";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F);
+            label5.Location = new Point(415, 200);
+            label5.Name = "label5";
+            label5.Size = new Size(60, 20);
+            label5.TabIndex = 6;
+            label5.Text = "Soyisim";
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Segoe UI", 18F);
+            textBox2.Location = new Point(415, 148);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(338, 49);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Font = new Font("Segoe UI", 18F);
+            textBox3.Location = new Point(12, 279);
+            textBox3.MaxLength = 11;
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(338, 49);
+            textBox3.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label6.Location = new Point(12, 248);
+            label6.Name = "label6";
+            label6.Size = new Size(192, 28);
+            label6.TabIndex = 8;
+            label6.Text = "TC kimlik numarası";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label7.Location = new Point(12, 370);
+            label7.Name = "label7";
+            label7.Size = new Size(82, 28);
+            label7.TabIndex = 10;
+            label7.Text = "E-Posta";
+            // 
+            // textBox4
+            // 
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Font = new Font("Segoe UI", 18F);
+            textBox4.Location = new Point(12, 401);
+            textBox4.MaxLength = 100;
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(338, 49);
+            textBox4.TabIndex = 9;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label8.Location = new Point(415, 370);
+            label8.Name = "label8";
+            label8.Size = new Size(175, 28);
+            label8.TabIndex = 12;
+            label8.Text = "İletişim numarası";
+            // 
+            // textBox5
+            // 
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Font = new Font("Segoe UI", 18F);
+            textBox5.Location = new Point(415, 401);
+            textBox5.MaxLength = 11;
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(338, 49);
+            textBox5.TabIndex = 11;
+            // 
+            // izinCombo
+            // 
+            izinCombo.Font = new Font("Segoe UI", 18F);
+            izinCombo.FormattingEnabled = true;
+            izinCombo.ItemHeight = 41;
+            izinCombo.Items.AddRange(new object[] { "Yıllık izin", "Doğum izni", "Süt izni", "Babalık izni", "Hastalık izni", "Ölüm izni", "Yeni iş arama izni", "Diğer" });
+            izinCombo.Location = new Point(12, 537);
+            izinCombo.Name = "izinCombo";
+            izinCombo.Size = new Size(338, 49);
+            izinCombo.TabIndex = 13;
+            izinCombo.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label9.Location = new Point(12, 506);
+            label9.Name = "label9";
+            label9.Size = new Size(113, 28);
+            label9.TabIndex = 14;
+            label9.Text = "İzin sebebi";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label10.Location = new Point(415, 507);
+            label10.Name = "label10";
+            label10.Size = new Size(100, 28);
+            label10.TabIndex = 16;
+            label10.Text = "İzin günü";
+            // 
+            // textBox6
+            // 
+            textBox6.BorderStyle = BorderStyle.FixedSingle;
+            textBox6.Font = new Font("Segoe UI", 18F);
+            textBox6.Location = new Point(415, 538);
+            textBox6.MaxLength = 11;
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(338, 49);
+            textBox6.TabIndex = 15;
+            // 
+            // yorumtxt
+            // 
+            yorumtxt.BorderStyle = BorderStyle.FixedSingle;
+            yorumtxt.Font = new Font("Segoe UI", 18F);
+            yorumtxt.Location = new Point(12, 630);
+            yorumtxt.MaxLength = 500;
+            yorumtxt.Multiline = true;
+            yorumtxt.Name = "yorumtxt";
+            yorumtxt.Size = new Size(741, 201);
+            yorumtxt.TabIndex = 13;
+            // 
+            // yorumlbl
+            // 
+            yorumlbl.AutoSize = true;
+            yorumlbl.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            yorumlbl.Location = new Point(12, 599);
+            yorumlbl.Name = "yorumlbl";
+            yorumlbl.Size = new Size(64, 28);
+            yorumlbl.TabIndex = 14;
+            yorumlbl.Text = "Diğer";
+            yorumlbl.Click += yorumlbl_Click;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label13.Location = new Point(387, 845);
+            label13.Name = "label13";
+            label13.Size = new Size(111, 28);
+            label13.TabIndex = 24;
+            label13.Text = "Bitiş tarihi";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label12.Location = new Point(12, 845);
+            label12.Name = "label12";
+            label12.Size = new Size(159, 28);
+            label12.TabIndex = 21;
+            label12.Text = "Başlangıç tarihi";
+            label12.Click += label12_Click_1;
+            // 
+            // endDatePicker
+            // 
+            endDatePicker.CalendarFont = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            endDatePicker.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            endDatePicker.Location = new Point(387, 876);
+            endDatePicker.MinDate = new DateTime(2024, 12, 10, 0, 0, 0, 0);
+            endDatePicker.Name = "endDatePicker";
+            endDatePicker.Size = new Size(366, 47);
+            endDatePicker.TabIndex = 23;
+            // 
+            // startDatePicker
+            // 
+            startDatePicker.CalendarFont = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            startDatePicker.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            startDatePicker.Location = new Point(12, 876);
+            startDatePicker.MinDate = new DateTime(2024, 12, 10, 0, 0, 0, 0);
+            startDatePicker.Name = "startDatePicker";
+            startDatePicker.Size = new Size(366, 47);
+            startDatePicker.TabIndex = 25;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LimeGreen;
+            button1.FlatAppearance.BorderColor = Color.White;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(274, 957);
+            button1.Name = "button1";
+            button1.Size = new Size(224, 86);
+            button1.TabIndex = 26;
+            button1.Text = "Gönder";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // LeaveRequestForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            ClientSize = new Size(790, 495);
+            Controls.Add(button1);
+            Controls.Add(startDatePicker);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(endDatePicker);
+            Controls.Add(label10);
+            Controls.Add(textBox6);
+            Controls.Add(label9);
+            Controls.Add(yorumtxt);
+            Controls.Add(yorumlbl);
+            Controls.Add(izinCombo);
+            Controls.Add(label8);
+            Controls.Add(textBox5);
+            Controls.Add(label7);
+            Controls.Add(textBox4);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "LeaveRequestForm";
+            Text = "LeaveRequestForm";
+            Load += LeaveRequestForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label8;
+        private TextBox textBox5;
+        private ComboBox izinCombo;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox6;
+        private TextBox yorumtxt;
+        private Label yorumlbl;
+        private Label label13;
+        private Label label12;
+        private DateTimePicker endDatePicker;
+        private DateTimePicker startDatePicker;
+        private Button button1;
     }
 }
--- HRMS.UI/Forms/LeaveRequestForm.cs
+++ HRMS.UI/Forms/LeaveRequestForm.cs
@@ -15,6 +15,75 @@ namespace HRMS.UI.Forms
         public LeaveRequestForm()
         {
             InitializeComponent();
+
+            startDatePicker.ValueChanged += (sender, e) =>
+            {
+                if (endDatePicker.Value < startDatePicker.Value)
+                {
+                    MessageBox.Show("Bitiş tarihi, başlangıç tarihinden önce olamaz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  
+                    endDatePicker.Value = startDatePicker.Value;
+                }
+            };
+
+            endDatePicker.ValueChanged += (sender, e) =>
+            {
+                if (endDatePicker.Value < startDatePicker.Value)
+                {
+                    MessageBox.Show("Bitiş tarihi, başlangıç tarihinden önce olamaz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                   
+                    endDatePicker.Value = startDatePicker.Value;
+                }
+            };
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LeaveRequestForm_Load(object sender, EventArgs e)
+        {
+            yorumtxt.Visible = false;
+            yorumlbl.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (izinCombo.SelectedIndex == 7)
+            {
+                yorumlbl.Visible = true;
+                yorumtxt.Visible = true;
+            }
+            else
+            { yorumtxt.Visible = false; yorumlbl.Visible = false; }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void yorumlbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
